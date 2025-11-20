6b3b059d2085fb618b7aa5f5b3777d9f4ac4b60b
Ravish <ravishranjan.rr@gmail.com>
2025-11-21 01:38:49 +0530

    Modify Account class and TransactionType enum

--- src/main/java/com/ApexLending/bank/exceptions/BankException.java
+++ src/main/java/com/ApexLending/bank/exceptions/BankException.java
@@ -1,7 +1,7 @@
 package main.java.com.ApexLending.bank.exceptions;
 
 public class BankException extends RuntimeException{
-	BankException(String msg){
+	public BankException(String msg){
 		super(msg);
 	}
 }
--- src/main/java/com/ApexLending/bank/model/Account.java
+++ src/main/java/com/ApexLending/bank/model/Account.java
@@ -2,40 +2,84 @@ package main.java.com.ApexLending.bank.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import main.java.com.ApexLending.bank.exceptions.BankException;
+
 public abstract class Account {
-	protected String accountId;
-	protected String customerId;
+	protected final String accountId;
+	protected final String customerId;
 	protected double balance;
 	protected boolean active;
-	protected LocalDate openedDate;
-	protected List<Transaction> transactions;
+	protected final LocalDate openedDate;
+	protected final List<Transaction> transactions = new ArrayList<>();
 
-	
-	
-	public Account(String accountId, String customerId, double balance, boolean active, LocalDate openedDate) {
+	public Account(String accountId, String customerId, double initial) {
 		this.accountId = accountId;
 		this.customerId = customerId;
-		this.balance = balance;
-		this.active = active;
-		this.openedDate = openedDate;
+		this.balance = initial;
+		this.active = true;
+		this.openedDate = LocalDate.now();
 	}
 
-	public void deposit(double amount,LocalDateTime timeStamp) {
-		balance = balance + amount;
-		transactions.add(new Transaction("TXN-DP", accountId, TransactionType.DEPOSIT, amount, timeStamp, "Deposit"));
-	}
-
-	public abstract void withdraw(double amount,LocalDateTime timeStamp);
-	
 	public String getAccountId() {
 		return accountId;
 	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
 	public double getBalance() {
 		return balance;
 	}
-	public List<Transaction> getTransactions(){
-		return transactions;
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void close() {
+		this.active = false;
+	}
+
+	protected void validateActive() throws BankException {
+		if (!active) {
+			throw new BankException("Account is closed");
+		}
+	}
+
+	public synchronized void deposit(double amount) throws BankException {
+		validateActive();
+		if (amount <= 0) {
+			throw new BankException("Amount must be positive");
+		}
+		balance = balance + amount;
+		transactions.add(new Transaction(accountId, TransactionType.DEPOSIT, amount, null));
+	}
+
+	public synchronized void withdraw(double amount) throws BankException {
+		validateActive();
+		if (amount <= 0) {
+			throw new BankException("Amount must be positive");
+		}
+		if (!canWithdraw(amount)) {
+			throw new BankException("Insufficient funds / overdraft limit exceeded");
+		}
+		balance = balance - amount;
+		transactions.add(new Transaction(accountId, TransactionType.WITHDRAWAL, amount, null));
+	}
+
+	protected abstract boolean canWithdraw(double amount);
+
+	public List<Transaction> getTransactions() {
+		return Collections.unmodifiableList(transactions);
+	}
+
+	@Override
+	public String toString() {
+		return accountId + " | " + customerId + " | " + this.getClass().getSimpleName() + " | " + balance + " | active="
+				+ active;
 	}
 }
