3b6a1ec79e848a3421e496cc4148df1fd412da43
Ravish <ravishranjan.rr@gmail.com>
2025-11-21 01:58:50 +0530

    modify ChackingAccount class

--- src/main/java/com/ApexLending/bank/model/CheckingAccount.java
+++ src/main/java/com/ApexLending/bank/model/CheckingAccount.java
@@ -1,26 +1,27 @@
 package main.java.com.ApexLending.bank.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import main.java.com.ApexLending.bank.exceptions.InsufficientFundsException;
 
 public class CheckingAccount extends Account {
 	private double overdraftLimit;
-	public CheckingAccount(String accountId, String customerId, double balance, boolean active, LocalDate openedDate, double overdraftLimit) {
-		super(accountId, customerId, balance, active, openedDate);
+
+	public CheckingAccount(String accountId, String customerId, double initial, double overdraftLimit) {
+		super(accountId, customerId, initial);
 		this.overdraftLimit = overdraftLimit;
 	}
 
-	
-
 	@Override
-	public void withdraw(double amount, LocalDateTime timeStamp) {
-		if (balance + overdraftLimit < amount) {
-			throw new InsufficientFundsException(amount);
+	protected boolean canWithdraw(double amount) {
+		if (balance + overdraftLimit >= amount) {
+			return true;
+		} else {
+			return false;
 		}
-		balance = balance - amount;
-		transactions.add(new Transaction("TXN-WD", accountId, TransactionType.WITHDRAWAL, amount, timeStamp, "Withdrawal"));
 	}
 
+	public double getOverdraftLimit() {
+		return overdraftLimit;
+	}
 }
